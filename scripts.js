6f18a88f559538d57a473400313739bb93e74877
LuhBC-pixel <bc.luisa@yahoo.com.br>
2021-02-03 15:32:10 -0300

    Implementa um botão para mudar o theme do site

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+const button = document.querySelector('#change-theme');
+
+button.addEventListener('click', () => {
+    document.body.classList.toggle('theme-dark')
+});
+
 const Modal = {
     open(){
         // Abrir modal
